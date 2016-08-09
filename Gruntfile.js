eb02c8bb6fc556750cbebf2988e1d06cfafedc87
Henry <hlewis@uchicago.edu>
2016-08-08 22:40:17 -0400

    sample gruntfile for standalone installation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = function(grunt) {
+
+  grunt.initConfig({
+    patternlab: {
+        options: {
+          // You can overwrite default config here
+        }
+    }
+  });
+
   grunt.loadTasks('tasks');
 };
