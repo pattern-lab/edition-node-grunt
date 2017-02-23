5ad5bf7158ae28176025f100de65a14e530eee42
Tim Berman <berman.tim@gmail.com>
2017-02-24 01:37:44 +1100

    Correct documentation around build command (#22)
    - Correct grunt config as it was not copying nested directories and files
    - Correct build command in documenation as `patternlab:build` does not copy files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,9 +71,9 @@ module.exports = function (grunt) {
       main: {
         files: [
           { expand: true, cwd: path.resolve(paths().source.js), src: '**/*.js', dest: path.resolve(paths().public.js) },
-          { expand: true, cwd: path.resolve(paths().source.css), src: '*.css', dest: path.resolve(paths().public.css) },
-          { expand: true, cwd: path.resolve(paths().source.images), src: '*', dest: path.resolve(paths().public.images) },
-          { expand: true, cwd: path.resolve(paths().source.fonts), src: '*', dest: path.resolve(paths().public.fonts) },
+          { expand: true, cwd: path.resolve(paths().source.css), src: '**/*.css', dest: path.resolve(paths().public.css) },
+          { expand: true, cwd: path.resolve(paths().source.images), src: '**/*', dest: path.resolve(paths().public.images) },
+          { expand: true, cwd: path.resolve(paths().source.fonts), src: '**/*', dest: path.resolve(paths().public.fonts) },
           { expand: true, cwd: path.resolve(paths().source.root), src: 'favicon.ico', dest: path.resolve(paths().public.root) },
           { expand: true, cwd: path.resolve(paths().source.styleguide), src: ['*', '**'], dest: path.resolve(paths().public.root) },
           // slightly inefficient to do this again - I am not a grunt glob master. someone fix
